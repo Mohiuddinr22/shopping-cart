b0110d1e3d4dab16b23ac1bfeef58486e2e73aed
Mohiuddin Mohi <mohiuddinr.22@gmail.com>
2025-10-07 07:22:53 +0600

    first commit

---
+++ src/dummy-products.js
@@ -0,0 +1,85 @@
+import mochaOvercoat from './assets/mocha-overcoat.jpg';
+import dreamGown from './assets/dream-gown.jpg';
+import rainJacket from './assets/rain-jacket.jpg';
+import merlotSuit from './assets/merlot-suit.jpg';
+import moonlightDress from './assets/moonlight-dress.jpg';
+import denimPioneer from './assets/denim-pioneer.jpg';
+import tangerineSilk from './assets/adiba.jpg';
+import sunshineRadiance from './assets/adiba1.jpg';
+import rustElegance from './assets/adiba2.jpg';
+
+export const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    image: mochaOvercoat,
+    title: 'Majestic Vintage Mocha Overcoat',
+    price: 129.99,
+    description:
+      'Channel timeless sophistication with this stunning mocha overcoat. Crafted for the discerning gentleman who appreciates the fine blend of vintage charm and modern elegance.',
+  },
+  {
+    id: 'p2',
+    image: dreamGown,
+    title: 'Enchanting Blush Dream Gown',
+    price: 189.99,
+    description:
+      'Bask in the glow of elegance with our Enchanting Blush Dream Gown. Embody the regality of a queen with a sweet touch of pink that whispers enchantment. This is the perfect piece for those seeking to create unforgettable moments.',
+  },
+
+  {
+    id: 'p3',
+    image: rainJacket,
+    title: 'Vintage Sunshine Rain Jacket',
+    price: 49.99,
+    description:
+      'Brace the showers in style! Our Vintage Sunshine Rain Jacket ensures that you stand out, even in the dullest weather. Because rain is never a reason to compromise on your fashion quotient.',
+  },
+  {
+    id: 'p4',
+    image: merlotSuit,
+    title: 'Classic Merlot Business Suit',
+    price: 249.99,
+    description:
+    'Step into the boardroom with unmatched confidence in our Classic Merlot Business Suit. Exuding an air of refined class and understated power, it is ideal for the modern executive who values tradition and elegance.',
+    },
+    {
+    id: 'p5',
+    image: moonlightDress,
+    title: 'Ethereal Moonlight Evening Dress',
+    price: 159.99,
+    description:
+    'Sweep the room off its feet in our Ethereal Moonlight Evening Dress. Crafted to mimic the allure of the moonlight, this dress is a nod to those who appreciate subtle glamour and a standout silhouette.',
+    },
+    {
+    id: 'p6',
+    image: denimPioneer,
+    title: 'Pioneer Rugged Denim Jacket',
+    price: 79.99,
+    description:
+    'Our Pioneer Rugged Denim Jacket is a tribute to those who embody the spirit of adventure. Designed with durability and comfort in mind, this jacket is a wardrobe essential for the urban explorer.'
+    },
+    {
+    id: 'p7',
+    image: tangerineSilk,
+    title: 'Tangerine Silk Grace Saree',
+    price: 179.99,
+    description:
+    'Delicately woven borders add a touch of refined artistry, while the smooth texture ensures effortless comfort throughout the day. Whether paired with traditional gold jewelry or modern minimal accessories, the Tangerine Silk Grace brings out your inner glow and charm.'
+    },
+    {
+      id: 'p8',
+      image: sunshineRadiance,
+      title: 'Sunshine Radiance Yellow Saree',
+      price: 279.99,
+      description: 
+      'Brighten your wardrobe with the Sunshine Radiance Yellow Saree, a symbol of warmth, positivity, and celebration. Woven with fine craftsmanship, this saree blends traditional elegance with modern charm. The vibrant yellow tone enhances every skin tone, making it a perfect choice for festive occasions, haldi ceremonies, or joyful daytime events.'
+    },
+    {
+      id: 'p9',
+      image: rustElegance,
+      title: 'Rust Elegance Dress',
+      price: 249.99,
+      description: 
+      'Our rust elegance dress is crafted from high-quality fabric with a soft, flowy silhouette, this dress offers both comfort and elegance. Perfect for evening gatherings, festive celebrations, or casual chic outings, it captures attention with its natural depth of color and graceful fall.'
+    }
+];
